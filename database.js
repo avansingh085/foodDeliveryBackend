8e5d45a55a630d1317ad402aea4c23cc3e4bfc57
Avan Singh <avansingh085@gmail.com>
2025-01-27 22:16:37 +0530

    ok

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 const db=()=>{
     try{
-       mongoose.connect("mongodb://127.0.0.1:27017/fooddb")
+       mongoose.connect("mongodb+srv://avansingh085:SbhUyHjWETMpJWUN@cluster0.tyyrk.mongodb.net/Ecommerce")
          .then(() => console.log("MongoDB connected"))
          .catch(err => console.error("MongoDB connection error:", err));
     }
