6326801ff7306571e9555984dd7ba8fa51c35ff0
Avan Singh <avansingh085@gmail.com>
2025-05-04 18:30:58 +0530

    update and commit

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const mongoose=require('mongoose');
 const db=()=>{
     try{
-   let url1="mongodb+srv://adityavishwakarma9025:ALnl1qBnxLrI05PO@cluster0.s4vzk.mongodb.net";
-   let url2="mongodb+srv://avansingh085:SbhUyHjWETMpJWUN@cluster0.tyyrk.mongodb.net";
+  
+   let url="mongodb+srv://avansingh085:SbhUyHjWETMpJWUN@cluster0.tyyrk.mongodb.net";
       //  mongodb+srv://adityavishwakarma9025:ALnl1qBnxLrI05PO@cluster0.s4vzk.mongodb.net/imageGalleria-db?retryWrites=true&w=majority&appName=Cluster0
-       mongoose.connect(`${url2}/fooddb`)
+       mongoose.connect(`${url}/fooddb`)
          .then(() => console.log("MongoDB connected"))
          .catch(err => console.error("MongoDB connection error:", err));
     }
